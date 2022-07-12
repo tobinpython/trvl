c122b88aa2d31ec7944ee6b30aeaef359a8dc8cc
tobinpython <tobinomc5@gmail.com>
2022-07-12 12:56:38 +0530

    trlproject

---
+++ tobinproject/tobinapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from. models import Place
+
+# Create your views here.
+
+def demo(request):
+     obj=Place.objects.all()
+     return render(request,"index.html",{'result':obj})
+
+
+
